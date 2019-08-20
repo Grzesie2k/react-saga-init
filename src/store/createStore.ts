5316fb83e2147ad07c58bae2fe8bdd6f8073499b
Grzegorz Kielak <grzegorz@kielak.me>
2019-08-20 13:42:57 +0200

    Add redux-saga

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,18 +1,27 @@
 import { routerMiddleware } from "connected-react-router"
 import { History } from "history";
 import { applyMiddleware, createStore, StoreEnhancer } from "redux";
-import createStoreReducer from "./createStoreReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga"
+import createStoreReducer from "./createStoreReducer";
+import storeSaga from "./storeSaga";
 
 export default async (history: History, debug: boolean) => {
     const storeReducer = createStoreReducer(history);
+    const sagaMiddleware = createSagaMiddleware();
+
     let storeEnhancer = applyMiddleware(
         routerMiddleware(history),
+        sagaMiddleware,
     ) as StoreEnhancer;
 
     if (debug) {
         storeEnhancer = composeWithDevTools(storeEnhancer);
     }
 
-    return createStore(storeReducer, storeEnhancer);
+    const store = createStore(storeReducer, storeEnhancer);
+
+    sagaMiddleware.run(storeSaga);
+
+    return store;
 };
