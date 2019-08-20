90393b7b5b5c41e94e49587e5369549c38d030d9
Grzegorz Kielak <grzegorz@kielak.me>
2019-08-20 13:35:48 +0200

    Add redux-devtools-extension

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,13 +1,18 @@
 import { routerMiddleware } from "connected-react-router"
 import { History } from "history";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, StoreEnhancer } from "redux";
 import createStoreReducer from "./createStoreReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export default (history: History) => {
+export default async (history: History, debug: boolean) => {
     const storeReducer = createStoreReducer(history);
-    const storeEnhancer = applyMiddleware(
+    let storeEnhancer = applyMiddleware(
         routerMiddleware(history),
-    );
+    ) as StoreEnhancer;
+
+    if (debug) {
+        storeEnhancer = composeWithDevTools(storeEnhancer);
+    }
 
     return createStore(storeReducer, storeEnhancer);
 };
