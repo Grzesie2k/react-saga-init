f85c886bf886fc142b651ed6c236e98614d1ec26
Grzegorz Kielak <grzegorz@kielak.me>
2019-08-21 10:48:22 +0200

    Show error screen on error in redux saga

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -3,12 +3,23 @@ import { History } from "history";
 import { applyMiddleware, createStore, StoreEnhancer } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga"
+import { ErrorScreenType } from "../components/ErrorScreen/model/ErrorScreenType";
+import { showErrorScreen } from "../components/ErrorScreen/store/errorScreenActions";
 import createStoreReducer from "./createStoreReducer";
 import storeSaga from "./storeSaga";
 
 export default (history: History, debug: boolean) => {
     const storeReducer = createStoreReducer(history);
-    const sagaMiddleware = createSagaMiddleware();
+
+    const sagaMiddleware = createSagaMiddleware({
+        onError(error, errorInfo): void {
+            console.group("Exception in redux-saga");
+            console.error(error);
+            console.info(errorInfo.sagaStack);
+            console.groupEnd();
+            store.dispatch(showErrorScreen(ErrorScreenType.CLIENT_ERROR));
+        },
+    });
 
     let storeEnhancer = applyMiddleware(
         routerMiddleware(history),
