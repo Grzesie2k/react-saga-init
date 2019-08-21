48fc18a99b47c4bbada9adb0880840a4f3a184e1
Grzegorz Kielak <grzegorz@kielak.me>
2019-08-21 17:16:34 +0200

    Call sessionPersister in preloadState

--- src/store/preloadState.ts
+++ src/store/preloadState.ts
@@ -1,6 +1,7 @@
 import { DeepPartial } from "redux";
+import sessionPersister from "../components/Session/services/sessionPersister";
 import { StoreState } from "./StoreState";
 
 export default (): DeepPartial<StoreState> => ({
-
+    session: sessionPersister.read(),
 });
