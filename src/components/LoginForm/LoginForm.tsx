619b735b4a2ddba9f8c821418bfc87f70dc2622d
Grzegorz Kielak <grzegorz@kielak.me>
2019-09-02 12:06:58 +0200

    Move card from login form to loginFormPage component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Form, Icon, Input } from "antd";
+import { Button, Form, Icon, Input } from "antd";
 import Checkbox from "antd/es/checkbox";
 import { FormComponentProps } from "antd/lib/form";
 import * as React from "react";
@@ -8,48 +8,46 @@ import styles from "./loginForm.module.css";
 
 const LoginForm: FC<FormComponentProps> = ({form}) => {
     return (
-        <Card title="Logowanie" className={styles.card}>
-            <Form>
-                <Form.Item>
-                    {form.getFieldDecorator("email", {
-                        rules: [requiredRule, emailRule],
-                    })(
-                        <Input
-                            type="email"
-                            autoComplete="username"
-                            placeholder="Adres e-mail"
-                            prefix={<Icon type="user" />}
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    {form.getFieldDecorator("password", {
-                        rules: [requiredRule]
-                    })(
-                        <Input
-                            type="password"
-                            autoComplete="current-password"
-                            placeholder="Hasło"
-                            prefix={<Icon type="lock" />}
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item className={styles.actions}>
-                    {form.getFieldDecorator("remember", {
-                        valuePropName: "checked",
-                        initialValue: true,
-                    })(
-                        <Checkbox>Zapmiętaj mnie</Checkbox>
-                    )}
-                    <a className={styles.forgot} href="/">
-                        Przypomnij hasło
-                    </a>
-                    <Button type="primary" htmlType="submit" className={styles.button}>
-                        Zaloguj się
-                    </Button>
-                </Form.Item>
-            </Form>
-        </Card>
+        <Form>
+            <Form.Item>
+                {form.getFieldDecorator("email", {
+                    rules: [requiredRule, emailRule],
+                })(
+                    <Input
+                        type="email"
+                        autoComplete="username"
+                        placeholder="Adres e-mail"
+                        prefix={<Icon type="user"/>}
+                    />
+                )}
+            </Form.Item>
+            <Form.Item>
+                {form.getFieldDecorator("password", {
+                    rules: [requiredRule]
+                })(
+                    <Input
+                        type="password"
+                        autoComplete="current-password"
+                        placeholder="Hasło"
+                        prefix={<Icon type="lock"/>}
+                    />
+                )}
+            </Form.Item>
+            <Form.Item className={styles.actions}>
+                {form.getFieldDecorator("remember", {
+                    valuePropName: "checked",
+                    initialValue: true,
+                })(
+                    <Checkbox>Zapmiętaj mnie</Checkbox>
+                )}
+                <a className={styles.forgot} href="/">
+                    Przypomnij hasło
+                </a>
+                <Button type="primary" htmlType="submit" className={styles.button}>
+                    Zaloguj się
+                </Button>
+            </Form.Item>
+        </Form>
     );
 };
 
--- src/components/LoginForm/LoginFormPage.tsx
+++ src/components/LoginForm/LoginFormPage.tsx
@@ -1,12 +1,16 @@
+import { Card } from "antd";
 import * as React from "react";
 import { FC } from "react";
 import BlankLayout from "../BlankLayout/BlankLayout";
 import LoginForm from "./LoginForm";
+import styles from "./loginFormPage.module.css";
 
 const LoginFormPage: FC = () => {
     return (
         <BlankLayout title="Logowanie">
-            <LoginForm/>
+            <Card title="Logowanie" className={styles.card}>
+                <LoginForm/>
+            </Card>
         </BlankLayout>
     );
 };
