074194a63c92f93a7a11257855bf8aab467dac51
Grzegorz Kielak <grzegorz@kielak.me>
2019-09-02 17:55:40 +0200

    Handle 404

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { hot } from "react-hot-loader";
+import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "../LoginFormPage/LoginFormPage";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 const App: React.FC = () => {
     return (
-        <LoginFormPage/>
+        <Switch>
+            <Route path="/login" component={LoginFormPage}/>
+            <Route component={PageNotFound} />
+        </Switch>
     );
 };
 
---
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,24 @@
+import { Button, Result } from "antd";
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import BlankLayout from "../BlankLayout/BlankLayout";
+import React from "react";
+
+const PageNotFound: FC = () => (
+    <BlankLayout title="Strona nie istnieje">
+        <Result
+            status="404"
+            title="404"
+            subTitle="Strona nie istnieje"
+            extra={
+                <Link to="/">
+                    <Button type="primary" icon="left">
+                        Powrót na stronę główną
+                    </Button>
+                </Link>
+            }
+        />
+    </BlankLayout>
+);
+
+export default PageNotFound;
