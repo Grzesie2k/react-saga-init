17fefccc10809a2e5692a1a5d8ccae109eb60a1c
Grzegorz Kielak <grzegorz@kielak.me>
2019-09-03 17:55:39 +0200

    Remove ErrorScreenContainer

--- src/components/ErrorScreen/withErrorScreen.tsx
+++ src/components/ErrorScreen/withErrorScreen.tsx
@@ -1,13 +1,30 @@
 import * as React from "react";
-import { ComponentType, FC } from "react";
-import ErrorScreenContainer from "./ErrorScreenContainer";
+import { ComponentType, FC, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StoreState } from "../../store/StoreState";
+import ErrorScreen from "./ErrorScreen";
+import { ErrorScreenType } from "./model/ErrorScreenType";
+import { hideErrorScreen, showErrorScreen } from "./store/errorScreenActions";
 
 export default function <Props>(WrappedComponnet: ComponentType<Props>) {
-    const Component: FC<Props> = (props) => (
-        <ErrorScreenContainer>
-            <WrappedComponnet {...props}/>
-        </ErrorScreenContainer>
-    );
+    const Component: FC<Props> = (props) => {
+        const error = useSelector<StoreState, ErrorScreenType | null>((state) => state.errorScreen);
+
+        const dispatch = useDispatch();
+        const showError = useCallback((type: ErrorScreenType) => {
+            dispatch(showErrorScreen(type));
+        }, [dispatch]);
+
+        const hideError = useCallback(() => {
+            dispatch(hideErrorScreen());
+        }, [dispatch]);
+
+        return (
+            <ErrorScreen error={error} hideError={hideError} showError={showError}>
+                <WrappedComponnet {...props}/>
+            </ErrorScreen>
+        );
+    };
 
     Component.displayName = `withErrorScreen(${Component.displayName || Component.name})`;
 
