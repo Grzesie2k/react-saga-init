da6cd3268429b10a60159540ef646b10d7f7d2f8
Grzegorz Kielak <grzegorz@kielak.me>
2019-09-04 13:55:08 +0200

    Set min height of main menu component

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,6 +1,7 @@
 import { Icon, Menu } from "antd";
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
+import styles from "./mainMenu.module.css";
 
 interface MainMenuProps {
     currentUrl: string;
@@ -8,7 +9,12 @@ interface MainMenuProps {
 
 const MainMenu: FC<MainMenuProps> = (props) => {
     return (
-        <Menu defaultSelectedKeys={['/']} selectedKeys={[props.currentUrl]} theme="dark">
+        <Menu
+            defaultSelectedKeys={['/']}
+            selectedKeys={[props.currentUrl]}
+            theme="dark"
+            className={styles.menu}
+        >
             <Menu.Item key="/">
                 <Link to="/">
                     <Icon type="home"/>
